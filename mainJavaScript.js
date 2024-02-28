24d3e4881483cd7fb4a5eacf809b1bafdd310d55
JRFFaza <JRFFaza@uclan.ac.uk>
2024-02-28 20:33:44 +0200

    Editing help/ about boxes

--- mainJavaScript.js
+++ mainJavaScript.js
@@ -5,34 +5,31 @@ function burgerMenuClick() {
     navElement.classList.toggle("nav--open");
     burgerMenuElement.classList.toggle("burgerMenu--open");
 }
-let box = document.getElementById("Modal");
 
-// to open the modal
 let help = document.getElementById("help");
-
-//span element to close the modal
-let span = document.getElementsByClassName("ToClose")[0];
-
-//when click 'help' to open the modal
-help.onclick = function() {
-    box.style.display = "block";
-}
-
-//when click 'X' to close the modal
-span.onclick = function() {
-    box.style.display = "none";
-
-}
-let box2 = document.getElementById("Modal2");
-
+let helpBox = document.getElementById("helpBox");
+let closeHelpBox = document.getElementById("closeHelpBox");
 let about = document.getElementById("about");
+let aboutBox = document.getElementById("aboutBox");
+let closeAboutBox = document.getElementById("closeAboutBox");
 
-let span2 = document.getElementsByClassName("ToClose2")[0];
+// Display help box when pressed
+help.onclick = function() {
+    helpBox.style.display = "block";
+}
 
+// Close help box when pressed
+closeHelpBox.onclick = function() {
+    helpBox.style.display = "none";
+}
+
+// Display about box when pressed
 about.onclick = function() {
-    box2.style.display = "block";
+    aboutBox.style.display = "block";
 }
 
-span2.onclick = function() {
-    box2.style.display = "none";
+// Close about box when pressed
+closeAboutBox.onclick = function() {
+    aboutBox.style.display = "none";
 }
+
