41efb8a8e05b86a79c5c815f65326f9da78fd7e7
JRFFaza <JRFFaza@uclan.ac.uk>
2024-03-10 18:25:05 +0200

    Implementing scoreboard

--- appJavaScript.js
+++ appJavaScript.js
@@ -69,3 +69,49 @@ window.onclick = function(event) {
         nameBox.style.display = "none";
     }
 }
+
+let scoreboard = document.getElementById("scoreboard");
+let scoreboardBox = document.getElementById("scoreboardBox");
+let closeScoreboardBox = document.getElementById("closeScoreboardBox");
+
+// Display scoreboard box when pressed
+scoreboard.onclick = function() {
+    scoreboardBox.style.display = "block";
+}
+
+// Close scoreboard box when pressed
+closeScoreboardBox.onclick = function() {
+    scoreboardBox.style.display = "none";
+}
+
+const TH_API_URL = "https://codecyprus.org/th/test-api/"; // the API base url
+/*This is a function to access the /leaderboard at the specified URL*/
+function getLeaderBoard(url) {
+    // create and invoke the http request
+    fetch(url, { method: "GET" })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            handleLeaderboard(json);
+        });
+}
+
+getLeaderBoard(TH_API_URL + "leaderboard?sorted");
+
+function handleLeaderboard(leaderboard) {
+    let options = { day: 'numeric', month: 'short', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    let html = ""; // used to include HTML code for the table rows
+    let leaderboardArray = leaderboard['leaderboard'];
+    for(const entry of leaderboardArray) {
+        let date = new Date(entry['completionTime']);
+        let formattedDate = date.toLocaleDateString("en-UK", options);
+        html += "<tr>" +
+            "<td>" + entry['player'] + "</td>" +
+            "<td>" + entry['score'] + "</td>" +
+            "<td>" + entry['completionTime'] + "</td>" +
+            "</tr>";
+    }
+
+    document.getElementById('scoreboardResults').innerHTML += html;
+
+}
