c9c7a74c7182770ce37bd4082e21a525092ec57e
JRFFaza <JRFFaza@uclan.ac.uk>
2024-03-11 18:04:39 +0200

    Implementing scoreboard

--- appJavaScript.js
+++ appJavaScript.js
@@ -84,7 +84,7 @@ closeScoreboardBox.onclick = function() {
     scoreboardBox.style.display = "none";
 }
 
-const TH_API_URL = "https://codecyprus.org/th/test-api/"; // the API base url
+const TH_API_URL = "https://codecyprus.org/th/api/"; // the API base url
 
 /*This is a function to access the /leaderboard at the specified URL*/
 function getLeaderBoard(url) {
@@ -97,20 +97,30 @@ function getLeaderBoard(url) {
         });
 }
 
-getLeaderBoard(TH_API_URL + "leaderboard?sorted");
+function getSession() {
+    let url = new URL(window.location.href);
+    return url.searchParams.get("session");
+}
+
+let session = getSession();
+
+let url = TH_API_URL + "leaderboard?sorted&session=" + session;
+console.log(url);
+
+getLeaderBoard(url);
+
+console.log(url);
 
 function handleLeaderboard(leaderboard) {
-    let options = { day: 'numeric', month: 'short', hour: '2-digit', minute: '2-digit', second: '2-digit' };
     let html = ""; // used to include HTML code for the table rows
     let leaderboardArray = leaderboard['leaderboard'];
     for(const entry of leaderboardArray) {
-        let date = new Date(entry['completionTime']);
-        let formattedDate = date.toLocaleDateString("en-UK", options);
         html += "<tr class='tableRow'>" +
             "<td class='tableData'>" + entry['player'] + "</td>" +
             "<td class='tableData'>" + entry['score'] + "</td>" +
             "<td class='tableData'>" + entry['completionTime'] + "</td>" +
             "</tr>";
     }
-    document.getElementById('scoreboardResults').innerHTML += html;
+    let leaderboardElement = document.getElementById('scoreboardResults');
+    leaderboardElement.innerHTML += html;
 }
