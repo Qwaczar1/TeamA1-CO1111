b18d1583a1a58071ce6106b601657aff645c9c06
JRFFaza <JRFFaza@uclan.ac.uk>
2024-03-19 21:31:12 +0200

    Implementing /leaderboard

--- appJavaScript.js
+++ appJavaScript.js
@@ -177,3 +177,32 @@ scoreboard.onclick = function() {
 closeScoreboardBox.onclick = function() {
     scoreboardBox.style.display = "none";
 }
+
+const TH_API_URL = "https://codecyprus.org/th/api/leaderboard?sorted"; // the API base url
+
+function getLeaderBoard(url) {
+    // create and invoke the http request
+    fetch(url, { method: "GET" })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            handleLeaderboard(json);
+        });
+}
+
+getLeaderBoard(TH_API_URL + getCookie('sessionID'));
+
+console.log(TH_API_URL + getCookie('sessionID'));
+
+function handleLeaderboard(leaderboard) {
+    let html = ""; // used to include HTML code for the table rows
+    let leaderboardArray = leaderboard['leaderboard'];
+    for(const entry of leaderboardArray) {
+        html += "<tr>" +
+            "<td class='tableData'>" + entry['player'] + "</td>" +
+            "<td class='tableData'>" + entry['score'] + "</td>" +
+            "<td class='tableData'>" + entry['completionTime'] + "</td>" +
+            "</tr>";
+    }
+    document.getElementById('test-results-table').innerHTML += html;
+}
