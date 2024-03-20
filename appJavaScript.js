cbfb0a7802ae9c1037532b143e30e942846a47a8
JRFFaza <JRFFaza@uclan.ac.uk>
2024-03-20 20:25:32 +0200

    Implementing /leaderboard

--- appJavaScript.js
+++ appJavaScript.js
@@ -1,5 +1,5 @@
 let treasureHuntID;
-const sessionID = getCookie('sessionID'); // Get session ID from cookie
+const sessionID = getCookie("sessionID"); // Get session ID from cookie
 let treasureHuntsElement = document.getElementById("treasureHuntsList");
 
 // Function to fetch treasure hunt challenges
@@ -83,20 +83,20 @@ function setCookie(cookieName, cookieValue, expireDays) {
 }
 
 // Function to get cookie value
-function getCookie(cname) {
-    let name = cname + "=";
+function getCookie(cookieName) {
+    let name = cookieName + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);
+    let cookieArray = decodedCookie.split(';');
+    for(let i = 0; i <cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
         }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
         }
     }
-    return "";
+    return null;
 }
 
 // Function to fetch questions
@@ -162,39 +162,55 @@ button.onclick = function(event) {
 closeButton.onclick = function() {
     nameBox.style.display = "none";
 }
+//
+// const TH_API_URL = "https://codecyprus.org/th/api/"; // the API base url
+//
+// function getLeaderBoard(url) {
+//     // create and invoke the http request
+//     fetch(url, { method: "GET" })
+//         .then(response => response.json())
+//         .then(json => {
+//             console.log(json);
+//             handleLeaderboard(json);
+//         });
+// }
+//
+// // console.log(TH_API_URL + "leaderboard?session=" + getCookie('sessionID') + "&sorted");
+//
+// function handleLeaderboard(leaderboard) {
+//     let options = { day: 'numeric', month: 'short', hour: '2-digit', minute: '2-digit',second: '2-digit' };
+//     let html = ""; // used to include HTML code for the table rows
+//     let leaderboardArray = leaderboard['leaderboard'];
+//     console.log(leaderboardArray);
+//     for(const entry of leaderboardArray) {
+//         let date = new Date(entry['completionTime']);
+//         let formattedDate = date.toLocaleDateString("en-UK", options);
+//
+//         html += "<tr>" +
+//             "<td class='tableData'>" + entry['player'] + "</td>" +
+//             "<td class='tableData'>" + entry['score'] + "</td>" +
+//             "<td class='tableData'>" + formattedDate + "</td>" +
+//             "</tr>";
+//     }
+//     let scoreboardTable = document.getElementById('scoreboardTable');
+//     scoreboardTable.innerHTML += html;
+// }
+var identifier = "leaderboard.html"; // Identifier for this HTML file
 
-const TH_API_URL = "https://codecyprus.org/th/api/"; // the API base url
+// Execute the script only if the identifier matches
+if (window.location.pathname.includes(identifier)) {
+    var urlParams = new URLSearchParams(window.location.search);
 
-function getLeaderBoard(url) {
-    // create and invoke the http request
-    fetch(url, { method: "GET" })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json);
-            handleLeaderboard(json);
-        });
-}
-
-let url = TH_API_URL + "leaderboard?sorted&session=" + getCookie('sessionID');
-getLeaderBoard(url);
-
-// console.log(TH_API_URL + "leaderboard?session=" + getCookie('sessionID') + "&sorted");
-
-function handleLeaderboard(leaderboard) {
-    let options = { day: 'numeric', month: 'short', hour: '2-digit', minute: '2-digit',second: '2-digit' };
-    let html = ""; // used to include HTML code for the table rows
-    let leaderboardArray = leaderboard['leaderboard'];
-    console.log(leaderboardArray);
-    for(const entry of leaderboardArray) {
-        let date = new Date(entry['completionTime']);
-        let formattedDate = date.toLocaleDateString("en-UK", options);
-
-        html += "<tr>" +
-            "<td class='tableData'>" + entry['player'] + "</td>" +
-            "<td class='tableData'>" + entry['score'] + "</td>" +
-            "<td class='tableData'>" + formattedDate + "</td>" +
-            "</tr>";
+    // Check if the 'session' parameter exists
+    if (!urlParams.has('session')) {
+        // Set the 'session' parameter with a value (sessionID variable assumed to be defined)
+        urlParams.set('session', sessionID);
     }
-    let scoreboardTable = document.getElementById('scoreboardTable');
-    scoreboardTable.innerHTML += html;
+
+    // Update the URL with the modified parameters
+    var updatedUrl = window.location.pathname + '?' + urlParams.toString();
+    document.getElementById('leaderboardID').setAttribute('href', updatedUrl);
 }
+
+// let url = TH_API_URL + "leaderboard?sorted&session=" + getCookie("sessionID");
+// getLeaderBoard(url);
