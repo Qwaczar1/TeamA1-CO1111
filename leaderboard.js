f82717726cc4cb10a2203dea0b3377257ef4b92e
JRFFaza <JRFFaza@uclan.ac.uk>
2024-03-21 15:09:31 +0200

    Editing leaderboard styling

--- leaderboard.js
+++ leaderboard.js
@@ -16,6 +16,7 @@ function getLeaderBoard(url) {
 function handleLeaderboard(leaderboard) {
     let options = { day: 'numeric', month: 'short', hour: '2-digit', minute: '2-digit',second: '2-digit' };
     let html = ""; // used to include HTML code for the table rows
+    let count = 1;
     let leaderboardArray = leaderboard['leaderboard'];
     console.log(leaderboardArray);
     for(const entry of leaderboardArray) {
@@ -23,14 +24,15 @@ function handleLeaderboard(leaderboard) {
         let formattedDate = date.toLocaleDateString("en-UK", options);
 
         html += "<tr>" +
-            "<td class='tableData'>" + entry['player'] + "</td>" +
+            "<td class='tableData'>" + count + ". " + entry['player'] + "</td>" +
             "<td class='tableData'>" + entry['score'] + "</td>" +
             "<td class='tableData'>" + formattedDate + "</td>" +
             "</tr>";
+        count++;
     }
     let scoreboardTable = document.getElementById('scoreboardTable');
     scoreboardTable.innerHTML += html;
 }
 
-let url = TH_API_URL + "leaderboard?sorted&limit=20&session=" + getCookie("sessionID");
+let url = TH_API_URL + "leaderboard?sorted&limit=30&session=" + getCookie("sessionID");
 getLeaderBoard(url);
