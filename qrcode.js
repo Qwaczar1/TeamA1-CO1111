2f72f522c83be1da505d50fa51f36833331a8211
JRFFaza <JRFFaza@uclan.ac.uk>
2024-03-26 18:40:29 +0200

    QR code fix

--- qrcode.js
+++ qrcode.js
@@ -11,18 +11,6 @@ var scanner = new Instascan.Scanner(opts);
 var cameras;
 var currentCameraIndex = 0;
 
-Instascan.Camera.getCameras().then(function (cameras) {
-    if (cameras.length > 0) {
-        scanner.start(cameras[0]);
-    }
-    else {
-        console.error('No cameras found.');
-        alert("No cameras found.");
-    }
-}).catch(function (e) {
-    console.error(e);
-});
-
 scanner.addListener('scan', function (content) {
     console.log(content);
     document.getElementById("content").innerHTML = content;
@@ -49,6 +37,17 @@ let closeQRCodeBox = document.getElementById("closeQRCodeBox");
 
 // Display QR Code box when QR Code Logo is clicked
 QRCodeLogo.onclick = function() {
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+        }
+        else {
+            console.error('No cameras found.');
+            alert("No cameras found.");
+        }
+    }).catch(function (e) {
+        console.error(e);
+    });
     QRCodeBox.style.display = "block";
     scanner.start(cameras[currentCameraIndex]); // Start scanning when QR Code box is displayed
 }
