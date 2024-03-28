106a40924002b944c9287c4a176f840fe9756b92
JRFFaza <JRFFaza@uclan.ac.uk>
2024-03-28 22:43:09 +0200

    small edits

--- index.js
+++ index.js
@@ -25,13 +25,3 @@ about.onclick = function() {
 closeAboutBox.onclick = function() {
     aboutBox.style.display = "none";
 }
-
-
-// function BurgerOpenClose() {
-//     var x = document.getElementById("navList");
-//     if (x.className === "closed") {
-//         x.className = "open";
-//     } else {
-//         x.className = "closed";
-//     }
-// }
